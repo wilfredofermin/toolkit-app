c55ea08cc7af115db864b1bff17e90bdaf586565
Wilfredo Fermin <wilfredoferminreyes@gmail.com>
2023-01-21 16:42:24 -0400

    Correcion error timeout logout

--- config/filament.php
+++ config/filament.php
@@ -89,6 +89,7 @@ return [
         'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
         'pages' => [
             // 'login' => \Filament\Http\Livewire\Auth\Login::class,
+            // Estableciendo login personalizado
             'login' => Login::class,
         ],
     ],
@@ -238,7 +239,7 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
         'max_content_width' => null,
         'notifications' => [
@@ -246,7 +247,7 @@ return [
             'alignment' => 'right',
         ],
         'sidebar' => [
-            'is_collapsible_on_desktop' => false,
+            'is_collapsible_on_desktop' => true,
             'groups' => [
                 'are_collapsible' => true,
             ],
